a28943b12ca4811445742377c24310403b5dee5f
Mod-Ramah <m.ramahi989@gmail.com>
2024-01-01 01:07:26 +0300

    .

--- ui/src/api.js
+++ ui/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getItem } from "./localStorage";
 
 const api = axios.create({
-    baseURL: 'http://real-estate-mern-full-stack-server.vercel.app'
+    baseURL: 'https://real-estate-mern-full-stack-server.vercel.app'
 })
 
 export const RegisterAdmin = async (data) => {
