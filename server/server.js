6aedc4b4241de4e1de3c60debe4262dabc624258
Mod-Ramah <m.ramahi989@gmail.com>
2024-01-01 00:17:35 +0300

    deploy to vercel(url)

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ if(process.env.NODE_ENV === 'development'){
 
 };
 
-app.use('/api/v1', router)
+app.use('https://real-estate-mern-full-stack-ui.vercel.app/', router)
 const mongooseUrl = process.env.Mongoose_Url;
 mongoose.connect(mongooseUrl, {
     useNewUrlParser:true,
