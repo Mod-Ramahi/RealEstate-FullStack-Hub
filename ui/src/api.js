60d1406b523093cae26db59cb37e281255a7ea28
Mod-Ramah <m.ramahi989@gmail.com>
2024-01-01 00:18:51 +0300

    deploy to vercel (url)

--- ui/src/api.js
+++ ui/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getItem } from "./localStorage";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/v1'
+    baseURL: 'http://real-estate-mern-full-stack-server.vercel.app'
 })
 
 export const RegisterAdmin = async (data) => {
