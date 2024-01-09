df9a8380fac311f8cb6bdce295e99caa3638f5c7
Mod-Ramah <m.ramahi989@gmail.com>
2024-01-09 19:06:57 +0300

    .

--- server/index.js
+++ server/index.js
@@ -18,18 +18,18 @@ app.use(cors());
 //   });
 
 app.use(bodyParser.json());
-// app.use(cors({origin: 'https://real-estate-mern-full-stack-ui.vercel.app' }));
-// app.use((req, res, next) => {
+app.use(cors({origin: 'https://real-estate-mern-full-stack-ui.vercel.app' }));
+app.use((req, res, next) => {
 // //   res.setHeader('Access-Control-Allow-Credentials', true)
 // //   res.setHeader('Access-Control-Allow-Origin', '*')
-//   res.setHeader('Access-Control-Allow-Origin',origin);
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//   )
-//     next();
-// });
+  res.setHeader('Access-Control-Allow-Origin',origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+    next();
+});
 
 // const originAllowed = 'https://real-estate-mern-full-stack-ui.vercel.app';
 // app.use(cors({
@@ -39,17 +39,17 @@ app.use(bodyParser.json());
 //     credentials: true,
 //   }));
 
-app.use((req, res, next) => {
-    // res.header('Access-Control-Allow-Credentials', true)
-    res.header('Access-Control-Allow-Origin', 'https://real-estate-mern-full-stack-ui.vercel.app')
-    res.header('Access-Control-Allow-Methods','HEAD', 'OPTIONS', 'GET', 'POST', 'PUT', 'DELETE')
-    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization')
-    res.header(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-      )
-    next()
-});
+// app.use((req, res, next) => {
+//     // res.header('Access-Control-Allow-Credentials', true)
+//     res.header('Access-Control-Allow-Origin', 'https://real-estate-mern-full-stack-ui.vercel.app')
+//     res.header('Access-Control-Allow-Methods','HEAD', 'OPTIONS', 'GET', 'POST', 'PUT', 'DELETE')
+//     res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization')
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//       )
+//     next()
+// });
 app.use([
     compression(),
     express.json(),
@@ -61,7 +61,7 @@ if(process.env.NODE_ENV === 'development'){
 
 };
 
-app.use('https://real-estate-server-app.vercel.app/', router)
+app.use('/', router)
 app.get('/', (req, res) => {
     console.log('check test route')
     res.send('check test route response')
