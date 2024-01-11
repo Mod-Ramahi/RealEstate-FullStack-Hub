4632f635d02e5873fab3cbdad4303f0422779c31
Mod-Ramah <m.ramahi989@gmail.com>
2024-01-11 04:03:29 +0300

    a

--- server/index.js
+++ server/index.js
@@ -107,7 +107,7 @@ mongoose.connect(mongooseUrl, {
 app.use((req, res, next) => {
     res.status(404).json('bad request')
 });
-app.use((error, req, res, nesxt) => {
+app.use((error, req, res, next) => {
     if (error.status) {
         res.status(error.status).json(error.message)
     } else {
